adc01909756f34105fe20154657c0d9b7c83f5c5
송민석 <heroicms@g.skku.edu>
2024-05-18 16:50:55 +0900

    feat : prototype for 100 brands

--- learn.py
+++ learn.py
@@ -1,16 +1,35 @@
+import os
 import tensorflow as tf
+import IPython
 from tensorflow.keras.layers import Dense, GlobalAveragePooling2D
 from tensorflow.keras.models import Model
 from tensorflow.keras.applications import ResNet50
+from tensorflow.keras.applications import MobileNet
 
-model = ResNet50(input_shape=(244, 244, 3), include_top=False, weights='imagenet')
-output = model.output
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 
-x = GlobalAveragePooling2D()(output)
-x = Dense(64, activation='relu')(x)
-output = Dense(3, activation='softmax', name='output')(x)
+gpus = tf.config.list_physical_devices('GPU')
+if gpus:
+    try:
+        tf.config.set_visible_devices(gpus[0], 'GPU')
+        tf.config.experimental.set_memory_growth(gpus[0], True)
+    except RuntimeError as e:
+        print(e)
+def model_builder(hp):
+    model = ResNet50(input_shape=(224, 224, 3), include_top=False, weights='imagenet')
+    output = model.output
 
-model = Model(inputs=model.input, outputs=output)
+    x = GlobalAveragePooling2D()(output)
+    hp_units = hp.Int('units', min_value = 32, max_value = 512, step = 32)
+    x = Dense(units = hp_units, activation='relu')(x)
+    output = Dense(100, activation='softmax', name='output')(x)
+
+    model = Model(inputs=model.input, outputs=output)
+
+    hp_lr = hp.Choice('learning_rate', values = [1e-2, 1e-3, 1e-4])
+
+    model.compile(optimizer = tf.keras.optimizers.Adam(learning_rate = hp_lr), loss = 'categorical_crossentropy', metrics = ['accuracy'])
+    return model
 
 from tensorflow.keras.utils import to_categorical
 from sklearn.model_selection import train_test_split
@@ -18,53 +37,51 @@ from sklearn.model_selection import train_test_split
 #라벨 제작
 
 import numpy as np
-import os
-import cv2
 
-filename_train = ["vita500", "cool", "teazle"]
-filename_test = []
+def transform_images(x_train):
+        x_train = tf.image.resize(x_train, (256, 256))
+        x_train = tf.image.random_crop(x_train, (224, 224, 3))
+        x_train = tf.image.random_flip_left_right(x_train)
+        x_train = tf.image.random_saturation(x_train, 0.6, 1.4)
+        x_train = tf.image.random_brightness(x_train, 0.4)
+        x_train = x_train / 255
+        return x_train
 
-train_image = []
-tr_im = []
-train_label = []
-test_image = []
-test_label = []
+def parse_tfrecord(tfrecord):
+    features = {'image/source_id': tf.io.FixedLenFeature([], tf.int64),
+                'image/filename': tf.io.FixedLenFeature([], tf.string),
+                'image/encoded': tf.io.FixedLenFeature([], tf.string)}
+    x = tf.io.parse_single_example(tfrecord, features)
+    x_train = tf.image.decode_jpeg(x['image/encoded'], channels=3)
+    x_train = transform_images(x_train)
+    y_train = x['image/source_id']
+    y_train = tf.one_hot(y_train, 100)
+    return x_train, y_train
 
-for i in range(len(filename_train)):
-    image_dir = f"./{filename_train[i]}/"
-    image_files = [os.path.join(image_dir, name) for name in os.listdir(image_dir) if name.endswith(('jpg'))]
+def parse_tfrecord_valid(tfrecord):
+    features = {'image/source_id': tf.io.FixedLenFeature([], tf.int64),
+                'image/filename': tf.io.FixedLenFeature([], tf.string),
+                'image/encoded': tf.io.FixedLenFeature([], tf.string)}
+    x = tf.io.parse_single_example(tfrecord, features)
+    x_train = tf.image.decode_jpeg(x['image/encoded'], channels=3)
+    x_train = tf.image.resize(x_train, (224, 224)
+    y_train = x['image/source_id']
+    y_train = tf.one_hot(y_train, 100)
+    return x_train, y_train
 
-    for file_path in image_files:
-        image = cv2.imread(file_path, cv2.IMREAD_COLOR)
-        if image is not None:
-            train_image.append(image)
-            train_label.append(i)
 
-train_image = np.array(train_image)
-train_label = np.array(train_label)
+train_data = tf.data.TFRecordDataset('train.tfrecord')
+train_data = train_data.map(parse_tfrecord)
 
-print(train_label)
+valid_data = tf.data.TFRecordDataset('valid.tfrecord')
+valid_data = valid_data.map(parse_tfrecord_valid)
 
-for i in range(len(filename_test)):
-    image_dir = f"./{filename_test[i]}/"
-    image_files = [os.path.join(image_dir, name) for name in os.listdir(image_dir) if name.endswith(('jpg'))]
+#train_data = train_data.shuffle(buffer_size=10000)
+train_data = train_data.batch(64)
+train_data = train_data.prefetch(buffer_size=tf.data.experimental.AUTOTUNE)
 
-    for file_path in image_files:
-        image = cv2.imread(file_path, cv2.IMREAD_COLOR)
-        if image is not None:
-            test_image.append(image)
-            test_label.append(i)
-
-test_image = np.array(test_image)
-test_label = np.array(test_label)
-
-train_result = to_categorical(train_label)
-#test_result = to_categorical(test_label)
-train_image, val_image, train_result, val_result = train_test_split(train_image, train_result, test_size = 0.15)
-
-print(train_image.shape, train_result.shape)
-print(val_image.shape, val_result.shape)
-print(val_result)
+valid_data = valid_data.batch(64)
+valid_data = valid_data.prefetch(buffer_size=tf.data.experimental.AUTOTUNE)
 
 #라벨 제작 끝
 
@@ -72,29 +89,28 @@ print(val_result)
 
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from tensorflow.keras.optimizers import Adam
-from tensorflow.keras.callbacks import ReduceLROnPlateau, EarlyStopping
+import kerastuner as kt
+# model.compile(optimizer=Adam(0.048), loss='categorical_crossentropy', metrics=['accuracy'])
 
-train_gen = ImageDataGenerator(
-        rotation_range=90,
-        rescale=1/255.0
-)
+tuner = kt.Hyperband(model_builder,
+        objective = 'val_accuracy',
+        max_epochs = 10,
+        factor = 3,
+        directory = './log/', 
+        project_name = 'kt_test')
 
-val_gen = ImageDataGenerator(rescale=1/255.0)
-test_gen = ImageDataGenerator(rescale=1/255.0)
+class ClearTrainingOutput(tf.keras.callbacks.Callback):
+    def on_train_end(*args, **kwargs):
+        IPython.display.clear_output(wait = True)
 
-flow_train = train_gen.flow(train_image, train_result, batch_size=64, shuffle=True)
-flow_val = val_gen.flow(val_image, val_result, batch_size=64, shuffle=False)
-#flow_test = test_gen.flow(test_image, test_result, batch_size=64, shuffle=False)
+tuner.search(train_data, epochs=10, validation_data=valid_data, callbacks=[ClearTrainingOutput()])
+best = tuner.get_best_hyperparameters(num_trials=1)[0]
+print(f"{best.get('units')} {best.get('learning_rate')}");
 
-model.compile(optimizer=Adam(0.001), loss='categorical_crossentropy', metrics=['accuracy'])
-
-
-reduce_lr_cb = ReduceLROnPlateau(monitor='val_loss', factor=0.2, patience=3, mode='min', verbose=1)
-stop_cb = EarlyStopping(monitor='val_loss', patience=5, mode='min', verbose=1)
-
-result = model.fit(flow_train, epochs=50, validation_data=flow_val, callbacks=[reduce_lr_cb, stop_cb])
+model = tuner.hypermodel.build(best)
+result = model.fit(train_data, epochs=50, validation_data=valid_data)
 
 #model.evaluate(flow_test)
 
 from tensorflow.keras.models import save_model
-model.save("model_1.1_3.h5")
+model.save("Resnet50_False_hyperband.h5")
