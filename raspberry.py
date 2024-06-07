cf5dfaffeb519f326f6bda8e639fd331ddca14ed
hogyeomchu <gyeom0808@gmail.com>
2024-06-07 21:53:10 +0900

    feat : raspberrypi complete

--- raspberry.py
+++ raspberry.py
@@ -17,20 +17,26 @@ with open("labels.txt", "r", encoding="utf-8") as file:
     labels = file.read().splitlines()
 with open("prices.txt", "r", encoding="utf-8") as file:
     prices = file.read().splitlines()
+with open("events.txt", "r", encoding="utf-8") as file:
+    events = file.read().splitlines()
 
-kit = ServoKit(channels=12)
-button_cam = Button(14)
-button_add = Button(15)
-button_rmv = Button(16)
-button_rst = Button(17)
+kit = ServoKit(channels=16)
+button_cam = Button(15)
+button_add = Button(18)
+button_rmv = Button(17)
+button_rst = Button(14)
 
 
-camera = Picamera2()
+picam2 = Picamera2()
+config = picam2.create_still_configuration()
+picam2.configure(config)
+picam2.start()
+
 total_price = 0
 
 def take_picture():
     image_path = "./predict.jpg"
-    camera.capture(image_path)
+    picam2.capture_file(image_path)
     return image_path
 
 def speak(text):
@@ -97,60 +103,62 @@ def braille(num, digit):
         kit.servo[digit + 1].angle = 30
         kit.servo[digit + 2].angle = 30
         kit.servo[digit + 3].angle = 135    
-        
 
 
-"""
 print("시작")
-picture = take_picture()
-img = predict.predict()
-prediction = model.predict(img)
-result = np.argmax(prediction)
-speak(labels[result])
-print(labels[result])
-
-total_price = total_price + int(prices[result])
-speak(prices[result] + "원")
-print(prices[result])
-mill = total_price // 10000
-thou = (total_price % 10000) // 1000
-hund = total_price % 1000
-braille(mill, 0)
-braille(thou, 4)
-braille(hund, 8)
-"""
-
-while True:
-    try:
-        while True:
-            print("시작")
-            if button_cam.is_pressed:
-                picture = take_picture()
-                img = predict.predict()
-                prediction = model.predict(img)
-                result = np.argmax(prediction)
-                speak(labels[result])
-                print(labels[result])
-                speak(prices[result] + "원")
-                print(prices[result])
-                while True:
-                    if button_add.is_pressed:
-                        total_price = total_price + int(prices[result])
-                        mill = total_price // 10000
-                        thou = (total_price % 10000) // 1000
-                        hund = total_price % 1000
-                        braille(mill, 0)
-                        braille(thou, 4)
-                        braille(hund, 8)
-                        speak("구매 완료")
-                        break
+try:
+    while True:
+        if button_cam.is_pressed:
+            picture = take_picture()
+            img = predict.predict()
+            prediction = model.predict(img)
+            result = np.argmax(prediction)
+            speak(labels[result])
+            print(labels[result])
+            speak(prices[result] + "원")
+            print(prices[result])
+            if events[result] == "1":
+                print("원플러스원")
+                speak("원플러스원 행사상품입니다.")
+            elif events[result] == "2":
+                print("투플러스원")
+                speak("투플러스원 행사상품입니다.")
+            while True:
+                if button_add.is_pressed:
+                    total_price = total_price + int(prices[result])
+                    mill = total_price // 10000
+                    thou = (total_price % 10000) // 1000
+                    hund = total_price % 1000
+                    braille(mill, 0)
+                    braille(thou, 4)
+                    braille(hund, 8)
+                    print("구매 완료")
+                    speak("구매 완료")
+                    print("total price : %d", total_price)
+                    break
                     
-                    if button_rmv.is_pressed:
-                        speak("구매 취소")
-                        break
+                if button_rmv.is_pressed:
+                    print("구매 취소")
+                    speak("구매 취소")
+                    print("total price : %d", total_price)
+                    break
 
-    except button_rst.is_pressed:
-        total_price = 0
-        for i in range(12):
-            kit.servo[i].angle = 30
-        speak("초기화 완료")
+                if button_rst.is_pressed:
+                    total_price = 0
+                    for i in range(12):
+                        kit.servo[i].angle = 30
+                    print("초기화 완료")
+                    speak("초기화 완료")
+                    print("total price : %d", total_price)
+                    break
+
+        if button_rst.is_pressed:
+                total_price = 0
+                for i in range(12):
+                    kit.servo[i].angle = 30
+                print("초기화 완료")
+                speak("초기화 완료")
+                print("total price : %d", total_price)
+
+except KeyboardInterrupt:
+    print("프로그램 종료")
