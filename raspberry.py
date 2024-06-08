8dbe489becca6429ba6a693135b64da91524d259
hogyeomchu <gyeom0808@gmail.com>
2024-06-08 15:14:54 +0900

    feat : raspberrypi motor final

--- raspberry.py
+++ raspberry.py
@@ -23,7 +23,7 @@ with open("events.txt", "r", encoding="utf-8") as file:
 kit = ServoKit(channels=16)
 button_cam = Button(15)
 button_add = Button(18)
-button_rmv = Button(17)
+button_rmv = Button(4)
 button_rst = Button(14)
 
 
@@ -51,58 +51,77 @@ def speak(text):
         time.sleep(1)
 
     pygame.mixer.quit()
+
+def servo(num, on):
+    if num == 6:
+        if on == 0:
+            kit.servo[num].angle = 180
+        elif on == 1:
+            kit.servo[num].angle = 75
     
+    elif num == 11:
+        if on == 0:
+            kit.servo[num].angle = 75
+        elif on == 1:
+            kit.servo[num].angle = 180
+
+    else:
+        if on == 0:
+            kit.servo[num].angle = 30
+        elif on == 1:
+            kit.servo[num].angle = 135
+
 def braille(num, digit):
     if num == 0:
-        kit.servo[digit + 0].angle = 135
-        kit.servo[digit + 1].angle = 30
-        kit.servo[digit + 2].angle = 135
-        kit.servo[digit + 3].angle = 135
+        servo(digit + 0, 0)
+        servo(digit + 1, 1)
+        servo(digit + 2, 1)
+        servo(digit + 3, 1)
     elif num == 1:
-        kit.servo[digit + 0].angle = 30
-        kit.servo[digit + 1].angle = 135
-        kit.servo[digit + 2].angle = 30
-        kit.servo[digit + 3].angle = 30
+        servo(digit + 0, 1)
+        servo(digit + 1, 0)
+        servo(digit + 2, 0)
+        servo(digit + 3, 0)
     elif num == 2:
-        kit.servo[digit + 0].angle = 30
-        kit.servo[digit + 1].angle = 135
-        kit.servo[digit + 2].angle = 30
-        kit.servo[digit + 3].angle = 135
+        servo(digit + 0, 1)
+        servo(digit + 1, 0)
+        servo(digit + 2, 1)
+        servo(digit + 3, 0)
     elif num == 3:
-        kit.servo[digit + 0].angle = 135
-        kit.servo[digit + 1].angle = 135
-        kit.servo[digit + 2].angle = 30
-        kit.servo[digit + 3].angle = 30
+        servo(digit + 0, 1)
+        servo(digit + 1, 1)
+        servo(digit + 2, 0)
+        servo(digit + 3, 0)
     elif num == 4:
-        kit.servo[digit + 0].angle = 135
-        kit.servo[digit + 1].angle = 135
-        kit.servo[digit + 2].angle = 135
-        kit.servo[digit + 3].angle = 30
+        servo(digit + 0, 1)
+        servo(digit + 1, 1)
+        servo(digit + 2, 0)
+        servo(digit + 3, 1)
     elif num == 5:
-        kit.servo[digit + 0].angle = 30
-        kit.servo[digit + 1].angle = 135
-        kit.servo[digit + 2].angle = 135
-        kit.servo[digit + 3].angle = 30
+        servo(digit + 0, 1)
+        servo(digit + 1, 0)
+        servo(digit + 2, 0)
+        servo(digit + 3, 1)
     elif num == 6:
-        kit.servo[digit + 0].angle = 135
-        kit.servo[digit + 1].angle = 135
-        kit.servo[digit + 2].angle = 30
-        kit.servo[digit + 3].angle = 135
+        servo(digit + 0, 1)
+        servo(digit + 1, 1)
+        servo(digit + 2, 1)
+        servo(digit + 3, 0)
     elif num == 7:
-        kit.servo[digit + 0].angle = 135
-        kit.servo[digit + 1].angle = 135
-        kit.servo[digit + 2].angle = 135
-        kit.servo[digit + 3].angle = 135
+        servo(digit + 0, 1)
+        servo(digit + 1, 1)
+        servo(digit + 2, 1)
+        servo(digit + 3, 1)
     elif num == 8:
-        kit.servo[digit + 0].angle = 30
-        kit.servo[digit + 1].angle = 135
-        kit.servo[digit + 2].angle = 135
-        kit.servo[digit + 3].angle = 135
-    else:
-        kit.servo[digit + 0].angle = 135
-        kit.servo[digit + 1].angle = 30
-        kit.servo[digit + 2].angle = 30
-        kit.servo[digit + 3].angle = 135    
+        servo(digit + 0, 1)
+        servo(digit + 1, 0)
+        servo(digit + 2, 1)
+        servo(digit + 3, 1)
+    elif num == 9:
+        servo(digit + 0, 0)
+        servo(digit + 1, 1)
+        servo(digit + 2, 1)
+        servo(digit + 3, 0)   
 
 
 print("시작")
@@ -118,10 +137,10 @@ try:
             speak(prices[result] + "원")
             print(prices[result])
             if events[result] == "1":
-                print("원플러스원")
+                print("1 + 1")
                 speak("원플러스원 행사상품입니다.")
             elif events[result] == "2":
-                print("투플러스원")
+                print("2 + 1")
                 speak("투플러스원 행사상품입니다.")
             while True:
                 if button_add.is_pressed:
@@ -129,9 +148,12 @@ try:
                     mill = total_price // 10000
                     thou = (total_price % 10000) // 1000
                     hund = total_price % 1000
-                    braille(mill, 0)
+                    braille(mill, 8)
+                    time.sleep(1)
                     braille(thou, 4)
-                    braille(hund, 8)
+                    time.sleep(1)
+                    braille(hund, 0)
+                    time.sleep(1)
                     print("구매 완료")
                     speak("구매 완료")
                     print("total price : %d", total_price)
@@ -147,18 +169,23 @@ try:
                     total_price = 0
                     for i in range(12):
                         kit.servo[i].angle = 30
+                        if i % 4 == 3:
+                            time.sleep(1)
                     print("초기화 완료")
                     speak("초기화 완료")
                     print("total price : %d", total_price)
                     break
 
         if button_rst.is_pressed:
-                total_price = 0
-                for i in range(12):
-                    kit.servo[i].angle = 30
-                print("초기화 완료")
-                speak("초기화 완료")
-                print("total price : %d", total_price)
+            total_price = 0
+            for i in range(12):
+                kit.servo[i].angle = 30
+                if i % 4 == 3:
+                    time.sleep(1)
+            print("초기화 완료")
+            speak("초기화 완료")
+            print("total price : %d", total_price)
 
 except KeyboardInterrupt:
+    picam2.stop()
     print("프로그램 종료")
